c535fd222c63e9f8ad76eac41b1cc5f2ce356073
llabake <lemboyelabake@gmail.com>
2018-12-13 13:11:55 +0100

    set up send grid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    redirect_to root_url and return unless activated?
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
+      :user_name      => ENV['user_name'],
+      :password       => ENV['password'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
