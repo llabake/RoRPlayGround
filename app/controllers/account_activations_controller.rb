7d459ba2aa61db9d63ba9a435192b14e31af320a
llabake <lemboyelabake@gmail.com>
2018-12-17 12:00:08 +0100

    implement SRP in the account controller method

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,12 +1,16 @@
 class AccountActivationsController < ApplicationController
+  before_action :find_user_from_url, only :edit
   def edit
-    user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.activate
-      log_in user
-      redirect_to user, flash: { success: "Account activated!" }
+    if @user && !@user.activated? && @user.authenticated?(:activation, params[:id])
+      activate_and_log_user_in
     else
       redirect_to root_url, flash: { danger: "Invalid activation link" }
     end
+  end
+
+  private
+
+  def find_user_from_url
+    @user = User.find_by(email: params[:email])
   end
 end
--- app/helpers/account_activations_helper.rb
+++ app/helpers/account_activations_helper.rb
@@ -1,2 +1,7 @@
 module AccountActivationsHelper
+  def activate_and_log_user_in
+    @user.activate
+    log_in @user
+    redirect_to @user, flash: { success: "Account activated!" }
+  end
 end
